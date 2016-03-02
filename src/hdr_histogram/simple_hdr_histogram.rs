25744bb0954b8f9552e08490a056996c8995181c
Octavian Geagla <ogeagla@gmail.com>
2016-03-02 09:41:00 -0700

    wip build lib and main crates

---
+++ src/hdr_histogram/simple_hdr_histogram.rs
@@ -0,0 +1,20 @@
+pub trait Histogram {
+    //instance method
+    fn inst_func(&self) -> &'static str;
+
+    //static method
+    fn static_fun(thing: &'static str) -> Self;
+}
+
+pub struct SimpleHdrHistogram { something: &'static str }
+
+impl Histogram for SimpleHdrHistogram {
+
+    fn inst_func(&self) -> &'static str {
+        "hello from instance"
+    }
+
+    fn static_fun(thing: &'static str) -> SimpleHdrHistogram {
+        SimpleHdrHistogram { something: "stuff" }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+extern crate hdr_histogram;
 
 fn main() {
+
+    let simple_histo = hdr_histogram::simple_hdr_histogram::SimpleHdrHistogram { something: "stuff" };
 
 }

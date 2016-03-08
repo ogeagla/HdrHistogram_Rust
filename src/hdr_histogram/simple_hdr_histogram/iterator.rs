e737ef1bc8c659f99e6d819e8ce3fe91f208584e
Octavian Geagla <ogeagla@gmail.com>
2016-03-08 10:52:57 -0700

    start from master anew

--- src/hdr_histogram/simple_hdr_histogram/iterator.rs
+++ src/hdr_histogram/simple_hdr_histogram/iterator.rs
@@ -47,15 +47,57 @@ pub struct BaseHistogramIterator<T: HistogramCount> {
     total_count_to_current_index: u64,
     total_value_to_current_index: u64,
     array_total_count: u64,
-    count_at_this_value: u64,
+    count_at_this_value: T,
     fresh_sub_bucket: bool,
     current_iteration_value: HistogramIterationValue,
     integer_to_double_value_conversion_ratio: u64,
 }
 
 impl<T: HistogramCount> BaseHistogramIterator<T> {
-    fn reset_iterator(&mut self, histogram: SimpleHdrHistogram<T>) {
-        //TODO
+    fn reset_iterator(mut self, histogram: SimpleHdrHistogram<T>) {
+        self.histogram = histogram;
+        self.saved_histogram_total_raw_count = self.histogram.get_count();
+        self.array_total_count = self.histogram.get_count();
+        self.integer_to_double_value_conversion_ratio = 0; //TODO should be self.histogram.integer_to_double_value_conversion_ratio
+        self.current_index = 0;
+        self.current_value_at_index = 0;
+        self.next_value_at_index = 1 << self.histogram.get_unit_magnitude();
+        self.prev_value_iterated_to = 0;
+        self.total_count_to_prev_index = 0;
+        self.total_count_to_current_index = 0;
+        self.total_value_to_current_index = 0;
+        self.count_at_this_value = T::zero();
+        self.fresh_sub_bucket = true;
+        self.current_iteration_value.reset();
+    }
+    fn exhausted_sub_buckets(&self) -> bool {
+        self.current_index >= 1000 //TODO should be self.histogram.get_counts_array_length()
+    }
+}
+
+impl <T: HistogramCount> Iterator for BaseHistogramIterator<T> {
+    type Item = HistogramIterationValue;
+    fn next(&mut self) -> Option<Self::Item> {
+        //combine Java's hasNext() and next()
+        //first check if has next
+        if self.histogram.get_count() != self.saved_histogram_total_raw_count {
+            //in Java, this threw a ConcurrentModificationException
+            return None
+        }
+        if self.total_count_to_current_index >= self.array_total_count {
+            //this means hasNext() returns false
+            return None
+        }
+
+        while ! self.exhausted_sub_buckets() {
+            self.count_at_this_value = match self.histogram.get_count_at_index(self.current_index) {
+                Ok(val) => val,
+                Err(err) => T::zero()
+            };
+        }
+
+        None
+
     }
 }
 
